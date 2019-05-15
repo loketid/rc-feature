d7509159dd77a5a8d4fe6cc77a3e447bdf385370
Rizki Maulana Akbar <rizkimaulanaakbar@outlook.com>
2019-05-15 16:05:31 +0700

    Replace implementation of RedisConnectionDriver from PRedis to phpredis

--- src/driver/RedisConnectionDriver.php
+++ src/driver/RedisConnectionDriver.php
@@ -2,32 +2,33 @@
 
 namespace RCFeature\Driver;
 
-use Predis\Client;
 use RCFeature\ConnectionDriver;
+use Redis;
 
 class RedisConnectionDriver implements ConnectionDriver {
     private $client;
     private $configKey;
 
     public function __construct(array $config, string $app_name = "rc-feature-client") {
-        $this->client = new Client($config);
+        $this->client = new Redis();
+        $this->client->connect($config['hostname'], $config['port'], $config['timeout']);
         $this->configKey = $app_name;
     }
 
     function fetch(): array {
-        return $this->client->hgetall($this->configKey);
+        return $this->client->hGetAll($this->configKey);
     }
 
     function disable(string $feature): bool {
-        return $this->client->hset($this->configKey, $feature, 0) == 1;
+        return $this->client->hSet($this->configKey, $feature, 0) == 1;
     }
 
     function enable(string $feature): bool {
-        return $this->client->hset($this->configKey, $feature, 1) == 1;
+        return $this->client->hSet($this->configKey, $feature, 1) == 1;
     }
 
     function update($config): bool {
-        return $this->client->hmset($this->configKey, $config) == 1;
+        return $this->client->hMSet($this->configKey, $config) == 1;
     }
 }
 
